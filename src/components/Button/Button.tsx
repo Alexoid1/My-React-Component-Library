e77b8b23dd48dc143648979c95fe8bcb6f2e6f25
Pablo Zambrano <alex_zam@hotmail.es>
2022-08-12 20:49:39 -0500

    adding styles to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Button.css';
 
 interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="buttonCreate" type="button">
+  <a className="anchorC" href="#">{props.label}</a>
+</button>;
 }
 
 export default Button;
