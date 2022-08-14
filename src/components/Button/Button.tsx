6d2b363283e71d75f896c9deefd65a0f9be033ba
Pablo Zambrano <alex_zam@hotmail.es>
2022-08-14 00:15:52 -0500

    delete style css use react styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Button.css';
+import React, { useState } from 'react';
+
 
 interface ButtonProps {
   label: string,
@@ -8,16 +8,51 @@ interface ButtonProps {
 
 
 
-const Button = (props: ButtonProps): JSX.Element =>  {
- if(props.variant==="cromatic"){
-    return <button className="buttonCreate" type="button">
-    <a className="anchorC" href="#">{props.label}</a>
+const Button = (props: ButtonProps) =>  {
+  const [hoverStyle, setHoverstyle] = useState({color:"white",position:"100%"})
+  const buttonCreate: React.CSSProperties = {
+    //display: "inherit",
+    //position: "relative",
+    fontSize: "18px", 
+    fontFamily: "Arial, Helvetica, sans-serif", 
+    fontWeight: "bolder", 
+    padding: "5px 14px", 
+    margin: "5px 25px", 
+    border: `3px solid ${hoverStyle.color}`, 
+    borderRadius: "10px",
+    background: hoverStyle.color
+  };
+
+  const anchorC: React.CSSProperties = {
+    position: "relative", 
+    display: "inline-block", 
+    color: "black", 
+    overflow: "hidden", 
+    background: "linear-gradient(to right, rgb(255, 196, 0), rgb(255, 102, 0), 50%, black 50%)", 
+    backgroundClip: "text", 
+    WebkitBackgroundClip: "text", 
+    WebkitTextFillColor: "transparent", 
+    backgroundSize: "200% 100%",
+    backgroundPosition: hoverStyle.position, 
+    transition: "background-position 275ms ease", 
+    textDecoration: "none"
+  }
+
+  function hoverInButton(){
+    setHoverstyle({color:"rgb(255, 196, 0",position: "0 100%"})
+  }
+
+  function hoverOutButton(){
+    setHoverstyle({color:"white",position: "100%"})
+  }
+
+  return <button style={buttonCreate} 
+    type="button" 
+    onMouseEnter={hoverInButton} 
+    onMouseOut={hoverOutButton}>
+    <a style={anchorC} href="#">{props.label}</a>
   </button>;
- }else if (props.variant === "simple"){
-  return <button type="button" className="searchBook">{props.label}</button>
- }else{
-  return <button type="button" className="searchBook">{props.label}</button>
- }
+
 }
 
 export default Button;
